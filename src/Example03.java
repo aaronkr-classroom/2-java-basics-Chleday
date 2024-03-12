1455e40eff687dde8da33cba0b5af7d9f333d2d6
Chleday <162276728+Chleday@users.noreply.github.com>
2024-03-12 17:26:05 +0900

    2과제제출

---
+++ src/Example03.java
@@ -0,0 +1,21 @@
+
+public class Example03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 4;
+		short s = 1;
+		
+		System.out.print("byte: " +b);
+		System.out.print("tShort: " +s);
+		System.out.print("tInt: " +i);
+		System.out.print("tFloat: " +f);
+		System.out.print("tDouble: " +d);
+		// Double: 1.2345678901234567E9
+		
+		System.out.println("\nChar2: " + c2);
+		char c3 = 89;
+		System.out.println("\nChar3: " + c3)
+	}
+
+}
